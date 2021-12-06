c6d3b5a4d3f90d2f5039770c36ad052e71953ee5
MatejRuzic <mruzic@dickeys.com>
2021-12-06 10:44:58 +0100

    Day 5. complete

--- E - Advent - 5.py
+++ E - Advent - 5.py
@@ -5,7 +5,7 @@ def part1(lines):
     if lines[x][0][0] == lines[x][1][0] or lines[x][0][1] == lines[x][1][1]:
       horVer.append(lines[x])
   
-  ocean = [['.'] * 999 for _ in range(1000)]
+  ocean = [['.'] * 1000 for _ in range(1000)]
 
   for x in range(len(horVer)):
     x1, x2 = horVer[x][0][0], horVer[x][1][0]
@@ -46,7 +46,90 @@ def part1(lines):
 
 
 def part2(lines):
-  pass
+  ocean = [['.'] * 1000 for _ in range(1000)]
+
+  for l in range(len(lines)):
+    x1, x2 = lines[l][0][0], lines[l][1][0]
+    y1, y2 = lines[l][0][1], lines[l][1][1]
+
+    if x1 == x2:
+      for y in range(min(y1, y2), max(y1, y2) + 1):
+        if str(ocean[x1][y]).isdigit():
+          ocean[x1][y] += 1
+        else:
+          ocean[x1][y] = 1
+    
+    elif y1 == y2:
+      for x in range(min(x1, x2), max(x1, x2) + 1):
+        if str(ocean[x][y1]).isdigit():
+          ocean[x][y1] += 1
+        else:
+          ocean[x][y1] = 1
+
+    else:
+      slope = (y2 - y1) / (x2 - x1)
+
+      if slope == 1 or slope == -1:
+        if x1 < x2 and y1 < y2:
+          x, y = x1, y1
+          
+          while x <= x2 and y <= y2:
+            if str(ocean[x][y]).isdigit():
+              ocean[x][y] += 1
+            else:
+              ocean[x][y] = 1
+            
+            x += 1
+            y += 1
+
+        elif x1 > x2 and y1 < y2:
+          x, y = x1, y1
+          
+          while x >= x2 and y <= y2:
+            if str(ocean[x][y]).isdigit():
+              ocean[x][y] += 1
+            else:
+              ocean[x][y] = 1
+            
+            x -= 1
+            y += 1
+
+        elif x1 < x2 and y1 > y2:
+          x, y = x1, y1
+          
+          while x <= x2 and y >= y2:
+            if str(ocean[x][y]).isdigit():
+              ocean[x][y] += 1
+            else:
+              ocean[x][y] = 1
+            
+            x += 1
+            y -= 1
+
+        elif x1 > x2 and y1 > y2:
+          x, y = x1, y1
+          
+          while x >= x2 and y >= y2:
+            if str(ocean[x][y]).isdigit():
+              ocean[x][y] += 1
+            else:
+              ocean[x][y] = 1
+            
+            x -= 1
+            y -= 1
+
+  # for row in ocean:
+  #   print(*row)
+
+  total = 0
+
+  for x in range(len(ocean)):
+    for y in range(len(ocean[x])):
+      if str(ocean[x][y]).isdigit():
+        if ocean[x][y] > 1:
+          total += 1
+
+  return total
 
 
 def main():
